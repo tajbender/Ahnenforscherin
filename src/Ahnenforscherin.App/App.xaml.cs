8267402504b70c2796f50c98f4e2ee90d4e0caeb
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-16 22:29:14 +0100

    Add WinUI3 App Skeleton

---
+++ src/Ahnenforscherin.App/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Ahnenforscherin.App;
+
+public partial class App : Application
+{
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        var window = new MainWindow();
+        window.Activate();
+    }
+}
---
+++ src/Ahnenforscherin.App/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml;
+
+namespace Ahnenforscherin.App;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+}
