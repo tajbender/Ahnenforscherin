f3a3be0dbd3f8b6243bed8eb43cd27b1201ad128
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-17 00:03:57 +0100

    Add `Workbench` Page

--- src/Ahnenforscherin.App/MainWindow.xaml.cs
+++ src/Ahnenforscherin.App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml;
-using System.Diagnostics;
+using Microsoft.UI.Xaml.Controls;
+using Ahnenforscherin.App.Views;
 
 namespace Ahnenforscherin.App;
 
@@ -7,11 +8,25 @@ public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
-        this.InitializeComponent();
+        InitializeComponent();
+        //ContentFrame.Navigate(typeof(Workbench));
     }
 
     private void InitializeComponent()
     {
-        Debug.Print("Hello World!\nHeureka.");
+
+    }
+
+    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.SelectedItem is NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "workbench":
+                    //ContentFrame.Navigate(typeof(Workbench));
+                    break;
+            }
+        }
     }
 }
