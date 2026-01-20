69c84348c1a4f7c841d907221e1da7d604cbf65c
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-20 23:13:31 +0100

    Add Crash Test Dummies

--- Ahnenforscherin/Views/WorkbenchPage.xaml.cs
+++ Ahnenforscherin/Views/WorkbenchPage.xaml.cs
@@ -1,17 +1,6 @@
-using Microsoft.UI.Xaml;
+using Ahnenforscherin.Models;
+using Ahnenforscherin.Repositories;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +15,12 @@ namespace Ahnenforscherin.Views
         public WorkbenchPage()
         {
             InitializeComponent();
+
+            var repo = new InMemoryPersonRepository();
+            var dummies = PersonFactory.CreateSampleFamily();
+
+            foreach (var p in dummies)
+                repo.Add(p);
         }
     }
 }
